41199224e3a3ed08aeb67ad29e54a0094605e46b
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-01-31 17:35:18 -0500

    Bug fixed

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,10 +9,9 @@ class TabLink {
     
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
-    // Using the Item element, create a new instance of the TabItem class
+    this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);    // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    this.tabItem = new TabItem(this.tabItem);
+    this.tabItem = new TabItem(this.item);
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => {this.select()});
   };
@@ -20,10 +19,10 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-    const links = document.querySelectorAll('.tabs-link');
+    const links = document.querySelectorAll('tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    Array.from(links).forEach(item => item.classList.remove('tabs-link-selected'));
+    links.forEach(item => item.classList.remove('tabs-link-selected'));
     // Add a class named "tabs-link-selected" to this link
     // this.element;
     this.element.classList.add('tabs-link-selected');
@@ -48,7 +47,7 @@ class TabItem {
     // Add a class named "tabs-item-selected" to this element
     //this.element;
     this.element.classList.add('tabs-item-selected');
-  }
+    }
 }
 
 /* START HERE: 
@@ -62,4 +61,4 @@ class TabItem {
 */
 
 //links = document.querySelectorAll();
-links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link)); 
+let links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link)); 
